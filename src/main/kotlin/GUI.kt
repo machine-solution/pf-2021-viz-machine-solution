ec40895db19f6b41c8858f237c3a4de483a7a040
Andrey Zaitsev <machine.solution353@gmail.com>
2021-10-14 23:19:23 +0300

    Add pieChart

--- src/main/kotlin/GUI.kt
+++ src/main/kotlin/GUI.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,24 +16,12 @@ import androidx.compose.ui.window.rememberWindowState
 fun gui() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Compose for Desktop",
-        state = rememberWindowState(width = 300.dp, height = 300.dp)
+        title = "viz",
+        state = rememberWindowState(width = 1000.dp, height = 700.dp)
     ) {
-        val count = remember { mutableStateOf(0) }
         MaterialTheme {
             Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                    onClick = {
-                        count.value++
-                    }) {
-                    Text(if (count.value == 0) "Hello World" else "Clicked ${count.value}!")
-                }
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                    onClick = {
-                        count.value = 0
-                    }) {
-                    Text("Reset")
-                }
+//
             }
         }
     }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,9 +9,10 @@ import java.awt.Dimension
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionAdapter
 import javax.swing.WindowConstants
+import kotlin.math.*
 
 fun main() {
-    gui()
+//    gui()
     createWindow("pf-2021-viz")
 }
 
@@ -32,11 +33,62 @@ fun createWindow(title: String) = runBlocking(Dispatchers.Swing) {
 
 class Renderer(val layer: SkiaLayer): SkiaRenderer {
     val typeface = Typeface.makeFromFile("fonts/JetBrainsMono-Regular.ttf")
-    val font = Font(typeface, 40f)
+    val font = Font(typeface, 16f)
     val paint = Paint().apply {
-        color = 0xff9BC730L.toInt()
+        color = 0xff000000L.toInt()
         mode = PaintMode.FILL
         strokeWidth = 1f
+    }
+
+    private fun pieChart(data: List<Float>, names: List<String>, canvas: Canvas)
+    {
+        // константы расположения рисунка
+        val spread = 5f
+        val leftBound = 100f
+        val rightBound = 300f
+        val upBound = 100f
+        val downBound = 300f
+        val xDescription = 350f
+        val yDescription = 50f
+        val sizeDescription = 10f
+        val sizeDescriptionBound = 3f
+        val yOffset = 25f
+
+        val cnt = (data.size - 1)
+        var i = 0
+        val sumData = data.sum()
+        var currentSum = 0f
+        canvas.drawCircle((leftBound + rightBound) * 0.5f, (upBound + downBound) * 0.5f,
+            (rightBound - leftBound) * 0.5f + spread * 2f,
+            Paint().setARGB(255,0,0,0).setStrokeWidth(0.5f * spread))
+        data.forEach{
+            // отрисовка диаграммы
+            canvas.drawArc(leftBound + spread * cos((currentSum + 0.5f * it)/sumData * PI * 2f).toFloat(),
+                upBound + spread * sin((currentSum + 0.5f * it)/sumData * PI * 2f).toFloat(),
+                rightBound + spread * cos((currentSum + 0.5f * it)/sumData * PI * 2f).toFloat(),
+                downBound + spread * sin((currentSum + 0.5f * it)/sumData * PI * 2f).toFloat(),
+                currentSum/sumData * 360f,
+                it/sumData * 360f,true,
+                Paint().setARGB(255,255 * i / cnt,255 * (cnt - i) / cnt,85*(i % 4)))
+
+            // отрисовка подписей
+            val rectBound = Rect(xDescription - sizeDescriptionBound,
+                yDescription + i * yOffset - sizeDescriptionBound,
+                xDescription + sizeDescription + sizeDescriptionBound,
+                yDescription + sizeDescription + i * yOffset + sizeDescriptionBound)
+            canvas.drawRect(rectBound, Paint().setARGB(255,0,0,0))
+            val rect = Rect(xDescription, yDescription + i * yOffset,
+                xDescription + sizeDescription, yDescription + sizeDescription + i * yOffset)
+            canvas.drawRect(rect, Paint().setARGB(255,255 * i / cnt,255 * (cnt - i) / cnt,85*(i % 4)))
+
+            canvas.drawString("${names[i]} - ${"%.${2}f".format(it / sumData * 100f)}%",
+                xDescription + sizeDescription * 2f,
+                yDescription + sizeDescription + i * yOffset,
+                font, Paint().setARGB(255,0,0,0))
+            currentSum += it
+            i++
+        }
+
     }
 
     override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
@@ -45,7 +97,12 @@ class Renderer(val layer: SkiaLayer): SkiaRenderer {
         val w = (width / contentScale).toInt()
         val h = (height / contentScale).toInt()
 
+
         // РИСОВАНИЕ
+        // Круговая диаграмма
+        pieChart(listOf(1f,2f,3f,4f,5f,6f,7f,8f,9f,10f,11f,12f,13f,14f,15f,16f,17f,18f,19f),
+            listOf("1", "2", "3","4", "5", "6","7", "8", "9", "10", "11", "12", "13","14", "15", "16","17", "18", "19"),
+            canvas)
 
         layer.needRedraw()
     }
